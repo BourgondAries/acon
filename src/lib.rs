5acb30545b15da0b88dd3829afe163c13743de3e
Bourgond Aries <macocio@gmail.com>
2016-04-22 22:03:47 +0200

    Make the tests work by changing parsing slightly.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::BTreeMap;
-use std::str::Chars;
+use std::str::CharIndices;
 
 /// Interface ////////////////////////////////////
 
@@ -23,70 +23,98 @@ pub fn parse(input: &str) -> Result<Table, String> {
 /// Parser //////////////////////////////////////
 
 struct Parser<'a> {
+	curchar: Option<char>,
 	current: usize,
-	input: &str,
+	input: &'a str,
+	iterator: CharIndices<'a>,
 }
 
 impl<'a> Parser<'a> {
 	fn new(input: &'a str) -> Parser<'a> {
 		Parser {
+			curchar: None,
 			current: 0usize,
 			input: input,
+			iterator: input.char_indices(),
 		}
+	}
+
+	fn get_current_char(&self) -> Option<char> {
+		self.curchar
+	}
+
+	fn consume_until_non_whitespace(&mut self) -> usize {
+		if let Some(ch) = self.get_current_char() {
+			if !ch.is_whitespace() {
+				return self.current;
+			}
+		}
+		while self.current < self.input.len() {
+			if let Some((o, ch)) = self.iterator.next() {
+				self.current = o;
+				self.curchar = Some(ch);
+				if !ch.is_whitespace() {
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+		self.current
+	}
+
+	fn consume_until_whitespace(&mut self) -> usize {
+		println!("Doesn't this fire?");
+		if let Some(ch) = self.get_current_char() {
+			println!("Here? {}", ch);
+			if ch.is_whitespace() {
+				return self.current;
+			}
+		}
+		while self.current < self.input.len() {
+			if let Some((o, ch)) = self.iterator.next() {
+				self.current = o;
+				self.curchar = Some(ch);
+				println!("HEY! {}", ch);
+				if ch.is_whitespace() {
+					println!("This fires");
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+		self.current
 	}
 
 	fn find_key(&mut self) -> (usize, usize) {
-		while self.current < self.input.len() {
-			if self.input[self.current].is_whitespace() {
-				self.current += 1;
-			} else {
-				break;
-			}
-		}
-
-		let begin = self.current;
-
-		while self.current < self.input.len() {
-			if !self.input[self.current].is_whitespace() {
-				self.current += 1;
-			} else {
-				break;
-			}
-		}
-
-		let end = self.current;
+		let begin = self.consume_until_non_whitespace();
+		let end = self.consume_until_whitespace();
 		(begin, end)
 	}
 
-	fn find_non_whitespace(&mut self) -> Option<char> {
-		self.iterator.peek();
-		while let Some(x) = self.iterator.next() {
-			if x.is_whitespace() {
-				continue;
-			} else {
-				return Some(x);
-			}
-		}
-		None
-	}
-
-	fn parse(&mut self) {
-		/*consume_whitespace();
-		loop {
-			capture_word();
-			capture_variable();
-		}
-		*/
-	}
-
-	fn get_key(&mut self) {
-	}
 }
 
 /// Tests ////////////////////////////////////////
 
 #[test]
 fn test_whitespace() {
-	let mut parser = Parser::new("Test");
-	assert!(parser.find_non_whitespace() == Some('T'));
+	let mut parser = Parser::new("Test d");
+	for i in 1..50 {
+		assert_eq!(parser.consume_until_non_whitespace(), 0);
+	}
+}
+
+#[test]
+fn test_whitespace2() {
+	let mut parser = Parser::new("Test derp");
+	for i in 1..50 {
+		assert_eq!(parser.consume_until_whitespace(), 4);
+	}
+}
+
+#[test]
+fn test_key_1() {
+	let mut parser = Parser::new("Test 'key'");
+	assert_eq!(parser.find_key(), (0, 4));
 }
